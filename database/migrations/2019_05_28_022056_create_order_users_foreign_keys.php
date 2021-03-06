556826eacb1e3ca0bcdaa2eba3966caae41dd22c
Adonis Almonte <adonis0444@gmail.com>
2021-03-06 00:28:47 -0500

    Modify Old migration

--- database/migrations/2019_05_28_022056_create_order_users_foreign_keys.php
+++ database/migrations/2019_05_28_022056_create_order_users_foreign_keys.php
@@ -17,7 +17,9 @@ class CreateOrderUsersForeignKeys extends Migration
             $table->unsignedBigInteger('orderID')->change();
             $table->unsignedBigInteger('userID')->change();
             $table->unsignedBigInteger('statusID')->change();
+        });
 
+        Schema::table('orderusers', function (Blueprint $table){
             $table->foreign('orderID')->references('id')->on('orders');
             $table->foreign('userID')->references('id')->on('users');
             $table->foreign('statusID')->references('id')->on('status');
